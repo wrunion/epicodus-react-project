03aa4f9adea9ca9244cee5a8e83572c599f5d977
wrunion <winterrunion@gmail.com>
2020-05-06 15:47:50 -0700

    Build out Form component.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,23 @@
 import React from 'react'
+import {v4 } from 'uuid';
 
 function Form(props) {
   const { handleSubmitCallback } = props;
 
   function handleSubmit(e) {
     e.preventDefault();
-    const { itemName, description, available, price, key} = e.target;
-    handleSubmitCallback({itemName, description, available, price, key});
+    const { itemName, description, available, price, id} = e.target;
+    handleSubmitCallback({itemName, description, available, price, id});
   } 
 
   return (
     <div className="Form">
       <form>
-        <input type="text" />
-        <button type="submit" onClick={handleSubmit}></button>
+        <input type="text" name="itemName" id="itemName" />
+        <input type="text" name="description" id="description" />
+        <input type="number" name="available" price="available" />
+        <input type="number" name="price" price="price" />
+        <button type="submit" onClick={handleSubmit} id={v4()}></button>
       </form>
     </div>
   )
