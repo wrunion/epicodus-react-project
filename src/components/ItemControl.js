d56c9cfe176e3c99dc9b08e690f146384f48a2dc
wrunion <winterrunion@gmail.com>
2020-05-06 16:57:16 -0700

    Add functionality to toggle between form showing and default view.

--- src/components/ItemControl.js
+++ src/components/ItemControl.js
@@ -4,23 +4,52 @@ import Form from './Form';
 
 class ItemControl extends Component {
   state = {
-    itemList: []
+    itemList: [],
+    formShowing: false
+  }
+
+  displayForm = () => {
+    this.setState({formShowing: true});
   }
 
   handleFormSubmit = (item) => {
     console.log(item);
-
-    this.setState({itemList: [...this.state.itemList, item]});
+    this.setState({itemList: [...this.state.itemList, item], formShowing: false});
   }
-
+  
   render() {
-    return (
+    // let currentDisplay;
+    //  const formShowing = `<Form handleSubmitCallback={this.handleFormSubmit} />`
+
+     return (
       <div className="ItemControl">
-        <Form handleSubmitCallback={this.handleFormSubmit} />
-        {/* <Form /> */}
-        {/* <AddItemForm /> */}
+        {this.state.formShowing === true ?
+          <Form handleSubmitCallback={this.handleFormSubmit} />
+          :  <React.Fragment>
+              <h2>There are no items to display.</h2>
+              <button onClick={this.displayForm}>Add Item</button>
+            </React.Fragment>}
       </div>
-    )
+    );
+
+    // if (this.state.itemList.length > 0) {
+    //   if (this.state.formShowing === true) { 
+    //     return (
+    //       <React.Fragment>
+    //         <Form handleSubmitCallback={this.handleFormSubmit} />
+    //       </React.Fragment> 
+    //     );
+    //   }
+
+
+    // } else {
+    //   return (
+    //     <div className="DefaultView">
+    //       <h2>There are no items to display.</h2>
+    //       <button onClick={this.displayForm}>Add Item</button>
+    //     </div>
+    //   )
+    // }
   }
 }
 
