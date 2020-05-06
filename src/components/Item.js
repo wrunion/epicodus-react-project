010462e64c1b285ed067d23a73cbb4efaf29953e
wrunion <winterrunion@gmail.com>
2020-05-06 15:36:19 -0700

    Item component complete.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 function Item(props) {
-  const { name, description, available, price, key } = props;
+  const { name, description, available, price, handleDetailClick, key } = props;
 
   return (
     <div className="Item">
         <h3>{name}</h3>
         <p><em>{description}</em></p>
         <p>Available: {available} | Price: {price}</p>
-        <button onClick={this.handleClick} id={key}>Details</button>
+        <button onClick={handleDetailClick} id={key}>Details</button>
     </div>
   );
 }
