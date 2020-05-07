479688a505cb1f0b4d885096a1ac5cfe83edc103
wrunion <winterrunion@gmail.com>
2020-05-07 15:16:44 -0700

    Updates

---
+++ precache-manifest.52e84511a2f41857d3f6199790f74717.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2bedbae9c5c0af21e360706694598c",
+    "url": "/epicodus-react-project/index.html"
+  },
+  {
+    "revision": "d576dd7c174fadc2b5ad",
+    "url": "/epicodus-react-project/static/css/main.4d119171.chunk.css"
+  },
+  {
+    "revision": "3f7a949069d58db2e0f8",
+    "url": "/epicodus-react-project/static/js/2.5c1561d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/epicodus-react-project/static/js/2.5c1561d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d576dd7c174fadc2b5ad",
+    "url": "/epicodus-react-project/static/js/main.46f056b2.chunk.js"
+  },
+  {
+    "revision": "2a1fa263a178e4359938",
+    "url": "/epicodus-react-project/static/js/runtime-main.c4a759a1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/animal-flashcards/precache-manifest.ecdd42d384e5743f8ee2ea60b100c644.js"
+  "/epicodus-react-project/precache-manifest.52e84511a2f41857d3f6199790f74717.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/animal-flashcards/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/epicodus-react-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
