421386baf752318b9ef373f997dd5037fee90a9f
wrunion <winterrunion@gmail.com>
2020-05-07 15:16:20 -0700

    Updates

---
+++ precache-manifest.ecdd42d384e5743f8ee2ea60b100c644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2cbb6878536435811e5c411dca923de",
+    "url": "/animal-flashcards/index.html"
+  },
+  {
+    "revision": "ab6fc13e6c668bd2d1c4",
+    "url": "/animal-flashcards/static/css/main.4d119171.chunk.css"
+  },
+  {
+    "revision": "3f7a949069d58db2e0f8",
+    "url": "/animal-flashcards/static/js/2.5c1561d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animal-flashcards/static/js/2.5c1561d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab6fc13e6c668bd2d1c4",
+    "url": "/animal-flashcards/static/js/main.4166a478.chunk.js"
+  },
+  {
+    "revision": "cda9f9971471b4845327",
+    "url": "/animal-flashcards/static/js/runtime-main.51400913.js"
+  }
+]);
