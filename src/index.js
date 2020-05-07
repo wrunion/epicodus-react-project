f8a8e9aa2d4fd6c71a540bfa8533b9d8aa7924c6
wrunion <winterrunion@gmail.com>
2020-05-07 13:29:19 -0700

    Add menu styling.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
