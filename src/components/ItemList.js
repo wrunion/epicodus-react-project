dc1f6c0c4ab14403986b986bfb26ce57eecfea5b
wrunion <winterrunion@gmail.com>
2020-05-08 12:13:02 -0700

    Add Semantic UI styling to ItemList.

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 
 class ItemList extends Component {
+  renderList() {
+    return this.props.items.map((item) => {
+      return (
+        <div className="Item ui card grid-item" key={item.id}>
+          <div className="content">
+            <h5 key={item.id}>{item.itemName}</h5>
+          </div>
+          <div className="right floated content">
+            <button className="ui button basic">
+              Details
+            </button>
+          </div>
+        </div>
+      );
+  });
+}
+
   render() {
     return (
       <div>
-        <h4>This will be the item list.</h4>
+        {this.renderList()}
       </div>
     )
   }
 }
 
-export default ItemList;
+const mapStateToProps = (state) => {
+  return {
+    items: state.items
+  }
+}
+
+export default connect(mapStateToProps)(ItemList);
