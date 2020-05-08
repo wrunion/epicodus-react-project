f4e0208517bf596c4801100b49dcf37eb977a781
wrunion <winterrunion@gmail.com>
2020-05-08 11:31:54 -0700

    Wrap App components in Provider tag.

--- src/components/ItemControl.js
+++ src/components/ItemControl.js
@@ -5,42 +5,6 @@ import ItemDetailView from './ItemDetailView';
 
 class ItemControl extends Component {
   state = {
-    itemList: [{
-      itemName: "Green Matcha",
-      description: "A healthy tea that gives you energy and focus",
-      available: 100,
-      price: 20, 
-      id: 1
-    },
-    {
-      itemName: "Lavender Blend",
-      description: "A soothing blend to wash away worries",
-      available: 100,
-      price: 15,
-      id: 2
-    },
-    {
-      itemName: "Chamomile",
-      description: "A relaxing tea to help you sleep", 
-      available: 100,
-      price: 10,
-      id: 3
-    }, 
-    {
-      itemName: "White Jasmine",
-      description: "A sweet blend for focus and health",
-      available: 100,
-      price: 25,
-      id: 4
-    },
-    {
-      itemName: "Earl Grey",
-      description: "Classic bergamont flavoring",
-      available: 100,
-      price: 10,
-      id: 5
-    }
-  ],
     formShowing: false,
     itemSelected: null
   }
