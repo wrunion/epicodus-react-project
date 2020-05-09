ca564efd57d0aa8d558cda3dd195cfee73cd922d
wrunion <winterrunion@gmail.com>
2020-05-08 19:40:09 -0700

    Built out ItemDetail component.

--- src/components/tutorial/ItemDetail.js
+++ src/components/tutorial/ItemDetail.js
@@ -7,10 +7,14 @@ const ItemDetail = (props) => {
       <h5>Choose an item for a detailed view</h5>
     );
   } else {
-    const { itemName } = props.item;
+    const { itemName, description, price, available } = props.item;
     return (
       <React.Fragment>
         <h1>{itemName}</h1>
+        <p><em>{description}</em></p>
+        <p>Price: ${price} | Available: {available}</p>
+        <button className="ui button mini green basic">Add To Cart</button>
+        <button className="ui button mini grey basic">Cancel</button>
       </React.Fragment>
     );
   }
