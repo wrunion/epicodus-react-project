645363f38179b4aa71ba732767abfb4b56655373
wrunion <winterrunion@gmail.com>
2020-05-15 16:52:44 -0700

    Add test file and first test.

---
+++ src/__tests__/reducers/itemListReducer.test.js
@@ -0,0 +1,12 @@
+import itemListReducer from '../../reducers/itemListReducer';
+
+describe('itemListReducer', () => {
+
+  test('Should return default state if no action is passed in', () => {
+    
+  })
+
+
+
+
+});
