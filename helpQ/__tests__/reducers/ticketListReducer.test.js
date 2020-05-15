5c27324d2c1f95bc00b7cc18ffa8e091b186ab52
wrunion <winterrunion@gmail.com>
2020-05-15 16:34:25 -0700

    Begin writing reducer tests.

--- helpQ/__tests__/reducers/ticketListReducer.test.js
+++ helpQ/__tests__/reducers/ticketListReducer.test.js
@@ -2,6 +2,5 @@ import ticketListReducer from '../..reducers/ticketListReducer';
 
 describe('ticketListReducer', () => {
   test('Should return default state if no action type is specified', () => {
-    
+    expect(ticketListReducer({}, {type: null})).toEqual({});
   });
-});
