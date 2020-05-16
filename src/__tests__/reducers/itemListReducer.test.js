6d5bef9f5a233c48ee30032bd7247cb6ceafa25e
wrunion <winterrunion@gmail.com>
2020-05-15 17:36:57 -0700

    Add reducer test to delete an item. Test red.

--- src/__tests__/reducers/itemListReducer.test.js
+++ src/__tests__/reducers/itemListReducer.test.js
@@ -11,6 +11,25 @@ describe('itemListReducer', () => {
     id: 1
   };
 
+  const currentState = {
+    itemList: [
+      {
+      name: "Green Matcha",
+      description: "A healthy tea that gives you energy and focus",
+      available: 100,
+      price: 20, 
+      id: 1
+      },
+      {
+      name: "Lavender Blend",
+      description: "A soothing blend to wash away worries",
+      available: 100,
+      price: 15,
+      id: 2
+      }
+    ]
+  }
+
   test('Should sucessfully add new item data to masterItemList', () => {
     const { name, description, available, price, id } = itemData;
     action = {
@@ -35,6 +54,25 @@ describe('itemListReducer', () => {
     });
   });
 
+  test('Should successfully delete an item', () => {
+    action = {
+      type: 'DELETE_TICKET',
+      id: 1
+    };
+    expect(itemListReducer(currentState, action)).toEqual({
+      itemList: [
+      {
+      name: "Lavender Blend",
+      description: "A soothing blend to wash away worries",
+      available: 100,
+      price: 15,
+      id: 2
+      }]
+    });
+
+
+
+  })
 
   test('Should return default state if no action is passed in', () => {
     expect(itemListReducer({}, {type: null})).toEqual({});
