04f40108f17ba657e6b68893c2f3a51126e0226f
wrunion <winterrunion@gmail.com>
2020-05-16 12:05:58 -0700

    Create red test for ADD_ITEM reducer.

--- src/__tests__/reducers/itemListReducer.test.js
+++ src/__tests__/reducers/itemListReducer.test.js
@@ -5,7 +5,26 @@ describe('reducer', () => {
     expect(reducer([],{type: null})).toEqual([]);
   });
 
+  let action;
+  const itemData = [
+    {
+      itemName: "Green Matcha",
+      description: "A healthy tea that gives you energy and focus",
+      available: 100,
+      price: 20, 
+      id: 1
+    },
+    {
+      itemName: "Lavender Blend",
+      description: "A soothing blend to wash away worries",
+      available: 100,
+      price: 15,
+      id: 2
+    }
+  ]  
+  test('Should add new item data to state array', () => {
+    expect(reducer([], {type: 'ADD_ITEM', data: {...itemData[0]} })).toEqual([{...itemData[0]}]);
+  });
 
 
-  
 });
