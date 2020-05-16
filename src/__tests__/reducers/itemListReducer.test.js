c83372aaf304871b074afb10eb5b7ae62eeb89b0
wrunion <winterrunion@gmail.com>
2020-05-16 11:59:25 -0700

    Create first reducer. Default test now green.

--- src/__tests__/reducers/itemListReducer.test.js
+++ src/__tests__/reducers/itemListReducer.test.js
@@ -1,8 +1,11 @@
-import reducer from '../reducers';
+import reducer from '../../reducers';
 
 describe('reducer', () => {
-  ('Should return an empty array if action type is null', () => {
-    expect(reducer([], {type: null})).toEqual([]);
+  test('should return empty array if no action type is given', () => {
+    expect(reducer([],{type: null})).toEqual([]);
   });
+
+
+
   
 });
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,6 @@
-
+export default (state ={}, action) => {
+  return state;
+}
 
 
 // export default (state = {}, action) => {
