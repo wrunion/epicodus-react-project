7f25294985feec200e359baee5df1dc8fdd5be78
wrunion <winterrunion@gmail.com>
2020-05-18 12:31:25 -0700

    Complete edit mode test suite.

--- src/__tests__/reducers/handlClickReducer.test.js
+++ src/__tests__/reducers/handlClickReducer.test.js
@@ -13,6 +13,7 @@ describe('handleClickReducer', () => {
   test('Should return an "editing" value of true if action specified is ENTER_EDIT_MODE', ()=> {
     expect(handleClickReducer({}, {type: 'ENTER_EDIT_MODE', data: {}})).toEqual({editing: true});
     expect(handleClickReducer({selectedItem: 1}, {type: 'ENTER_EDIT_MODE', data: {}})).toEqual({selectedItem: 1, editing: true});
+    expect(handleClickReducer({editing: false, selectedItem: 2}, {type: 'ENTER_EDIT_MODE', data: {}})).toEqual({editing: true, selectedItem: 2});
   });
 
 });
