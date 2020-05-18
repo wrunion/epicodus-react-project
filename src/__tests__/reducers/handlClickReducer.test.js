8baf6ef03d3f7f94d20cb2f819bd84af8247708f
wrunion <winterrunion@gmail.com>
2020-05-18 12:38:46 -0700

    Tests and reducers complete.

--- src/__tests__/reducers/handlClickReducer.test.js
+++ src/__tests__/reducers/handlClickReducer.test.js
@@ -1,21 +1,24 @@
 import handleClickReducer from '../../reducers/handleClickReducer';
 
 describe('handleClickReducer', () => {
-
+  /* Default test */
   test('Should return default state if no action is specified', () => {
     expect(handleClickReducer({}, {type: null})).toEqual({});
   });
 
+  /* Cancel click should exit edit mode */
   test('Should return an "editing" value of false if action specified is CANCEL_CLICK', () => {
     expect(handleClickReducer({}, { type: 'CANCEL_CLICK', data: {} })).toEqual({ editing: false});
   });
 
+  /* Edit button should enter edit mode */
   test('Should return an "editing" value of true if action specified is ENTER_EDIT_MODE', ()=> {
     expect(handleClickReducer({}, {type: 'ENTER_EDIT_MODE', data: {}})).toEqual({editing: true});
     expect(handleClickReducer({selectedItem: 1}, {type: 'ENTER_EDIT_MODE', data: {}})).toEqual({selectedItem: 1, editing: true});
     expect(handleClickReducer({editing: false, selectedItem: 2}, {type: 'ENTER_EDIT_MODE', data: {}})).toEqual({editing: true, selectedItem: 2});
   });
 
+  /* Selecting an item should pass that item to selectedItem state slice */
   test('Should return selected item object if action type is SELECT_ITEM_CLICK', () => {
     expect(handleClickReducer({}, {type: 'SELECT_ITEM_CLICK', data: {      
     itemName: "Lavender Blend",
