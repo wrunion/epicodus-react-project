130bafb2e3457c07b34524ba780e5f4da4a92f2f
wrunion <winterrunion@gmail.com>
2020-05-18 12:10:25 -0700

    Add test for cancel click method. Test red.

--- src/__tests__/reducers/handlClickReducer.test.js
+++ src/__tests__/reducers/handlClickReducer.test.js
@@ -5,4 +5,8 @@ describe('handleClickReducer', () => {
   test('Should return default state if no action is specified', () => {
     expect(handleClickReducer([])).toEqual([]);
   });
+
+  test('Should return an "editing" value of null if action specified is CANCEL_CLICK', () => {
+    expect(handleClickReducer({}, 'CANCEL_CLICK')).toEqual({ editing: false});
+  });
 });
