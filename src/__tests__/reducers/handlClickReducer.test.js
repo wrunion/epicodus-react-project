c4596ce1a5ddd238e2d3d73fc0c937b9f07a7c86
wrunion <winterrunion@gmail.com>
2020-05-18 12:20:09 -0700

    Add reducer for cancel click. Test now green.

--- src/__tests__/reducers/handlClickReducer.test.js
+++ src/__tests__/reducers/handlClickReducer.test.js
@@ -3,10 +3,10 @@ import handleClickReducer from '../../reducers/handleClickReducer';
 describe('handleClickReducer', () => {
 
   test('Should return default state if no action is specified', () => {
-    expect(handleClickReducer([])).toEqual([]);
+    expect(handleClickReducer({}, {type: null})).toEqual({});
   });
 
   test('Should return an "editing" value of null if action specified is CANCEL_CLICK', () => {
-    expect(handleClickReducer({}, 'CANCEL_CLICK')).toEqual({ editing: false});
+    expect(handleClickReducer({}, { type: 'CANCEL_CLICK', data: {} })).toEqual({ editing: false});
   });
 });
--- src/reducers/handleClickReducer.js
+++ src/reducers/handleClickReducer.js
@@ -1,15 +1,14 @@
 export default(state={}, action) => {
-  // const { type, data } = action;
+  const { type, data } = action;
 
-  // switch(type) {
-  //   case 'CANCEL_CLICK': 
-  //     return state;
-  //   case 'SELECT_ITEM_CLICK':
-  //     return state; 
-  //   case 'ENTER_EDIT_CLICK': 
-  //     return state; 
-  //   default:
-  //     return state;  
-  // }
-  return state;
+  switch(type) {
+    case 'CANCEL_CLICK': 
+      return { editing: false };
+    case 'SELECT_ITEM_CLICK':
+      return state; 
+    case 'ENTER_EDIT_CLICK': 
+      return state; 
+    default:
+      return state;  
+  }
 }
