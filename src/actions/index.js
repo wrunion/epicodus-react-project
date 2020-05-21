a99f666a626a2aac701e0db4e7c99702e7baf545
wrunion <winterrunion@gmail.com>
2020-05-20 23:39:59 -0700

    Simplify actions and reducers.

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,37 +5,33 @@ export const addItem = (item) => {
     data: item
   };
 };
-export const editItem = (item) => {
-  return {
-    type: 'EDIT_ITEM',
-    data: item
-  };
-};
-export const deleteItem = (item) => {
-  return {
-    type: 'DELETE_ITEM',
-    data: item
-  };
-};
+// export const editItem = (item) => {
+//   return {
+//     type: 'EDIT_ITEM',
+//     data: item
+//   };
+// };
+// export const deleteItem = (item) => {
+//   return {
+//     type: 'DELETE_ITEM',
+//     data: item
+//   };
+// };
 
 /* Actions for handleClickReducer */
-export const cancelClick = (event) => {
-  return {
-    type: 'CANCEL_CLICK',
-    data: event
-  };
-};
+// export const cancelClick = (event) => {
+//   return {
+//     type: 'CANCEL_CLICK',
+//     data: event
+//   };
+// };
 export const selectItem = (item) => {
   return {
     type: 'ITEM_SELECTED',
     data: item
   };
 };
-export const enterEditMode = () => {
-  return {
-    type: 'ENTER_EDIT_MODE'
-  };
-};
+
 export const toggleForm = () => {
   return {
     type: 'TOGGLE_FORM'
--- src/reducers/selectedItemReducer.js
+++ src/reducers/selectedItemReducer.js
@@ -1,15 +1,9 @@
-export default(state={formShowing: false, selectedItem: null}, action) => {
+export default(state={selectedItem: null}, action) => {
   const { type, data } = action;
 
   switch(type) {
-    case 'CANCEL_CLICK': 
-      return { ...state, formShowing: false };
     case 'SELECT_ITEM_CLICK':
-      return { ...state,  selectedItem: data}; 
-    case 'ENTER_EDIT_MODE': 
-      return { ...state, editing: true };
-    case 'TOGGLE_FORM':
-      return { formShowing: !state.formShowing };  
+      return { ...state,  selectedItem: data};  
     default:
       return state;  
   }
