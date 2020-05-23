0de7584b00e36ce3d1ecdeae0d701253bf4ed3f6
wrunion <winterrunion@gmail.com>
2020-05-23 15:03:57 -0700

    Add FormControl component.

---
+++ src/components/FormControl.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import { showForm, hideForm } from './../actions';
+import Form from './Form';
+import PropTypes from 'prop-types';
+
+class FormControl extends React.Component {
+
+  handleCancelClick = () => {
+    this.props.hideForm(); 
+  }
+
+  render() {
+    return ( 
+      <div className="FormControl">
+        {this.props.formShowing
+        ? <Form handleCancelClick={this.handleCancelClick}/> 
+        : <button 
+            onClick={() => this.props.showForm()}>Add Item</button>}
+      </div>
+    );
+  }
+}
+
+FormControl.propTypes = {
+  showForm: PropTypes.func,
+  formShowing: PropTypes.bool
+};
+
+const mapStateToProps = state => {
+  return {
+    formShowing: state.formReducer.formShowing
+  }
+}
+
+export default connect(mapStateToProps, { showForm, hideForm }) (FormControl);
