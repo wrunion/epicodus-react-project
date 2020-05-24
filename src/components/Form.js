2476a1896cc3113c479445f0f561b11a9a86c01a
wrunion <winterrunion@gmail.com>
2020-05-23 18:34:40 -0700

    Add padding to Form.

--- src/components/Form.js
+++ src/components/Form.js
@@ -17,7 +17,8 @@ function Form(props) {
 
   return (
     <div className="Form">
-      <form id={v4()} onSubmit={handleSubmit} className="ui segment">
+      <form id={v4()} onSubmit={handleSubmit} className="ui segment TeaForm">
+        <h3>Add Item</h3>
         <div className="inputField">
           <label>Item Name: <input type="text" name="itemName" id="itemName" defaultValue="Green Tea" /></label>
         </div>
